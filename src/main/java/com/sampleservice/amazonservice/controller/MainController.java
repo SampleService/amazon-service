c73fd704027f56b0ef738d36d5321e1f0aefea4a
SeptemberHX <september_hx@outlook.com>
2019-12-03 16:56:16 +0800

    f

--- src/main/java/com/sampleservice/amazonservice/controller/MainController.java
+++ src/main/java/com/sampleservice/amazonservice/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.sampleservice.amazonservice.controller;
 
+import com.sampleservice.amazonservice.utils.MBaseUtils;
 import com.septemberhx.common.base.MResponse;
 import com.septemberhx.mclient.annotation.MApiFunction;
 import com.septemberhx.mclient.annotation.MRestApiType;
@@ -20,6 +21,10 @@ public class MainController extends MObject {
     public MResponse marketFunction(@RequestBody MResponse requestData) {
         MResponse result = new MResponse();
         result.set("msg", "/market");
+
+        if (requestData.get("interval") != null) {
+            MBaseUtils.generateStringInKBSize(2000, result, (int) requestData.get("interval"));
+        }
         return result;
     }
 }
